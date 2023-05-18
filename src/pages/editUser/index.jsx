4b098096a11ccab45d047c8699ed17575955dbf2
panbibi <1265629139@qq.com>
2023-05-18 15:59:15 +0800

    fix:编辑个人信息接口联调及数据回显 添加token判断

--- src/pages/editUser/index.jsx
+++ src/pages/editUser/index.jsx
@@ -1,22 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input, Button, Avatar } from '@nutui/nutui-react-taro';
 import { View, Text, Image } from '@tarojs/components';
+import { useDispatch, useSelector } from 'react-redux';
+import Taro from '@tarojs/taro';
 import './index.scss';
 
 const Index = () => {
-  const [avatar, setAvatar] = useState('');
+  const dispatch = useDispatch();
+  const userInfo = Taro.getStorageSync('userInfo');
+  const { userInfos } = useSelector((state) => state.my);
+  useEffect(() => {
+    const token = Taro.getStorageSync('token');
+    if (token !== '') {
+      dispatch({
+        type: 'my/getUserInfos',
+        payload: {
+          id: userInfo.id,
+        },
+      });
+    }
+    // eslint-disable-next-line global-require
+  }, []);
+  const [avatar, setAvatar] = useState(userInfos.headUrl);
+  const [nickname, setNickname] = useState(userInfos.nickName);
+
+  // 获取头像
   const onChooseavatar = (e) => {
     let { avatarUrl } = e.detail;
     setAvatar(avatarUrl);
   };
 
+  // 提交
+  const onOk = async () => {
+    await dispatch({
+      type: 'my/editUserInfo',
+      payload: {
+        id: userInfo.id,
+        headUrl: avatar,
+        nickName: nickname,
+      },
+    });
+    wx.navigateBack({
+      delta: 1,
+    });
+    await dispatch({
+      type: 'my/getUserInfos',
+      payload: {
+        id: userInfo.id,
+      },
+    });
+  };
+
   return (
     <View>
-      <View className="head-photo">
-        <View className="head-tex">
-          <Text className="head-text">头像</Text>
-        </View>
-        <View>
+      <Input
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+        label="头像"
+        placeholder=" "
+        clearable
+        inputAlign="right"
+        slotButton={
           <Button
             size="small"
             open-type="chooseAvatar"
@@ -27,18 +73,27 @@ const Index = () => {
               <Avatar icon="my" class="avatar" iconSize="16" bgColor="#fff" />
             ) : (
               <Image
-                style={{ width: 30, height: 30, background: '#fff' }}
+                style={{ width: 24, height: 24, background: '#fff' }}
                 class="avatar"
                 src={avatar}
               />
             )}
           </Button>
-        </View>
-      </View>
-      <Input label="昵称" type="nickname" placeholder="请输入昵称" inputAlign="right" />
+        }
+      />
+      <Input
+        label="昵称"
+        type="nickname"
+        placeholder="请输入昵称"
+        defaultValue={nickname}
+        inputAlign="right"
+        onChange={(val) => {
+          setNickname(val);
+        }}
+      />
       <Text className="foot">昵称2-32个字符，一个汉字为2个字符</Text>
       <View className="foot-btn">
-        <Button block color="#B08B57">
+        <Button block color="#B08B57" onClick={() => onOk()}>
           确认
         </Button>
       </View>
--- src/server/my.js
+++ src/server/my.js
@@ -1,6 +1,11 @@
 import request from '../utils/request';
 
-// 查询商品列表
+// 查询个人信息
 export const getUserInfo = (params) => {
-  return request.post(`/jcgl-mall/app/user/center/select/user/details`, params);
+  return request.post(`/jcgl-user/wx/login/selectUserInfo`, params);
+};
+
+// 编辑个人信息
+export const editUserInfo = (params) => {
+  return request.post(`/jcgl-user/wx/login/update`, params);
 };
