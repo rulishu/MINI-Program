3d9b218e61c11e2a55bdd3d630b9f024c57b04cf
panbibi <1265629139@qq.com>
2023-05-18 17:33:17 +0800

    fix:单个界面顶部导航栏样式调整

--- src/pages/addAddress/index.config.js
+++ src/pages/addAddress/index.config.js
@@ -1,3 +1,5 @@
 export default definePageConfig({
   navigationBarTitleText: '添加收货地址',
+  navigationBarBackgroundColor: '#ffffff',
+  navigationBarTextStyle: 'black',
 });
--- src/pages/editUser/index.config.js
+++ src/pages/editUser/index.config.js
@@ -1,3 +1,5 @@
 export default definePageConfig({
   navigationBarTitleText: '个人信息',
+  navigationBarBackgroundColor: '#ffffff',
+  navigationBarTextStyle: 'black',
 });
