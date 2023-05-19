a6f4335b3f325f61bf882b7a2052c75fc7f62587
rulishu <1324214367@qq.com>
2023-05-19 15:06:13 +0800

    fix: 调用商品详情接口

--- src/pages/goodInfo/card.jsx
+++ src/pages/goodInfo/card.jsx
@@ -3,23 +3,34 @@ import { View, Image, Text } from '@tarojs/components';
 import { Icon } from '@nutui/nutui-react-taro';
 // import Taro from '@tarojs/taro';
 import './index.scss';
+import { useSelector } from 'react-redux';
 
 const Index = () => {
+  const { queryInfo } = useSelector((state) => state.goodInfo);
+
   return (
     <View style={{ backgroundColor: '#ffffff' }}>
       <View className="cardHeader">
-        <Icon name="rect-left" size="20" style={{ marginRight: 10 }}></Icon>
+        <Icon name="rect-left" size="20" style={{ marginRight: 10, color: '#A0A1B4' }}></Icon>
         <Image
           mode="widthFix"
           // eslint-disable-next-line global-require
-          src={require('@/assets/images/wine.png')}
+          src={queryInfo?.mainGraph}
           className="cardHeaderImage"
         ></Image>
         <Icon name="rect-right" size="20" style={{ marginLeft: 10 }}></Icon>
       </View>
       <View className="cardImages">
-        <View>
+        {queryInfo?.skuImages?.map((item, idx) => (
           <Image
+            key={idx}
+            mode="widthFix"
+            // eslint-disable-next-line global-require
+            src={item}
+            className="cardImagesItem"
+          />
+        ))}
+        {/* <Image
             mode="widthFix"
             // eslint-disable-next-line global-require
             src={require('@/assets/images/home8.png')}
@@ -42,14 +53,7 @@ const Index = () => {
             // eslint-disable-next-line global-require
             src={require('@/assets/images/home8.png')}
             className="cardImagesItem"
-          />
-          <Image
-            mode="widthFix"
-            // eslint-disable-next-line global-require
-            src={require('@/assets/images/home8.png')}
-            className="cardImagesItem"
-          />
-        </View>
+          /> */}
         <View className="cardImagesTextBox">
           <Text className="cardImagesText"> 1/7</Text>
         </View>
--- src/pages/goodInfo/heads.jsx
+++ src/pages/goodInfo/heads.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
-import { View, Text } from '@tarojs/components';
-import { Icon } from '@nutui/nutui-react-taro';
+import React, { useEffect } from 'react';
+import { View, Text, Image } from '@tarojs/components';
+// import { Icon, } from '@nutui/nutui-react-taro';
 import Taro from '@tarojs/taro';
 import './index.scss';
+import { useDispatch } from 'react-redux';
 
 const Index = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({
+      type: 'goodInfo/infoDetails',
+      payload: {
+        id: 160,
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <View className="heads">
       <View className="title">
-        <View style={{ marginRight: 19 }}>
-          <Icon name="rect-left" size="15" onClick={() => Taro.navigateBack({ delta: 1 })}></Icon>
-        </View>
+        <Image
+          mode="widthFix"
+          // eslint-disable-next-line global-require
+          src={require('@/assets/images/left.png')}
+          className="headsTitleImages"
+          onClick={() => Taro.navigateBack({ delta: 1 })}
+        ></Image>
         <View>
           <Text className="titleText">奋斗之露 · 喜庆</Text>
         </View>
       </View>
       <View className="detailIcon">
-        <View style={{ marginRight: 19 }}>
-          <Icon name="share-n" size="15"></Icon>
-        </View>
-        <View style={{ marginRight: 19 }}>
-          <Icon name="message" size="15"></Icon>
-        </View>
-        <View>
-          <Icon name="cart" size="15"></Icon>
-        </View>
+        <Image
+          mode="widthFix"
+          // eslint-disable-next-line global-require
+          src={require('@/assets/images/share.png')}
+          className="detailIconOne"
+        ></Image>
+        <Image
+          mode="widthFix"
+          // eslint-disable-next-line global-require
+          src={require('@/assets/images/message.png')}
+          className="detailIconTwo"
+        ></Image>
+        <Image
+          mode="widthFix"
+          // eslint-disable-next-line global-require
+          src={require('@/assets/images/shop.png')}
+          className="detailIconThird"
+        ></Image>
       </View>
     </View>
   );
