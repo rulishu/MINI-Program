fd5cbd7b54ae6eaf62f00192e31ae871e037bbd7
rulishu <1324214367@qq.com>
2023-05-24 14:47:21 +0800

    fix: 选中标签颜色改变

--- src/pages/categories/right.jsx
+++ src/pages/categories/right.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { View, Text, Image } from '@tarojs/components';
 import { Tag, Price, Icon } from '@nutui/nutui-react-taro';
-// import Taro from '@tarojs/taro';
+import Taro from '@tarojs/taro';
 import './index.scss';
 
 const Index = (props) => {
   const { getCategoriesTwoTreeItem } = props;
   // const { subList } = useSelector((state) => state.categories);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [activeItem, setActiveItem] = useState(0);
 
   return (
     <View className="right" style={{ paddingBottom: 70, margin: -15 }}>
       <View style={{ marginBottom: 8 }} className="right-title-box">
         {getCategoriesTwoTreeItem?.map((itm, idx) => {
           return (
-            <View key={idx}>
+            <View
+              key={idx}
+              onTap={() => {
+                setActiveItem(idx);
+              }}
+            >
               <Text
                 className="right-title"
-                style={{ background: getCategoriesTwoTreeItem ? 'rgb(245, 245, 245)' : '' }}
+                style={{
+                  background:
+                    getCategoriesTwoTreeItem && idx !== activeItem ? 'rgb(245, 245, 245)' : '',
+                }}
               >
                 {itm?.label}
               </Text>
@@ -30,30 +39,71 @@ const Index = (props) => {
         {getCategoriesTwoTreeItem?.map((item, idx) => {
           return (
             <View key={idx} className="right-content">
-              <View className="right-content-title">
-                <Text>{item?.label}</Text>
-              </View>
-              <View className="right-content-box">
-                <View className="right-content-item">
-                  <Image
-                    mode="widthFix"
-                    // eslint-disable-next-line global-require
-                    src={require('@/assets/images/home8.png')}
-                    className="rightImage"
-                  ></Image>
-                  <View className="right-content-text-box">
-                    <View className="right-content-text-header">
-                      <Tag>自营</Tag>
-                      <Text>秋然长粒香大米 5kg/袋</Text>
-                    </View>
-                    <View className="right-content-text-tag">返 ¥9.10</View>
-                    <View className="right-content-text-footer">
-                      <Price price={11} size="normal" needSymbol thousands />
-                      <Icon name="cart" size={15}></Icon>
+              <View className="right-content-title">{item?.label}</View>
+              <scroll-view scroll-y="true" scroll-with-animation="true" style={{}}>
+                <View className="right-content-box">
+                  <View className="right-content-item">
+                    <Image
+                      mode="widthFix"
+                      // eslint-disable-next-line global-require
+                      src={require('@/assets/images/homebg.png')}
+                      className="rightImage"
+                      onTap={() => {
+                        dispatch({
+                          type: 'goodInfo/infoDetails',
+                          payload: {
+                            id: item?.id,
+                          },
+                        });
+                        Taro.navigateTo({ url: '/pages/goodInfo/index' });
+                      }}
+                    ></Image>
+                    <View className="right-content-text-box">
+                      <View
+                        className="right-content-text-header"
+                        onTap={() => {
+                          dispatch({
+                            type: 'goodInfo/infoDetails',
+                            payload: {
+                              id: item?.id,
+                            },
+                          });
+                          Taro.navigateTo({ url: '/pages/goodInfo/index' });
+                        }}
+                      >
+                        <Tag color="rgb(170, 170, 170)">自营</Tag>
+                        <Text style={{ marginLeft: 10 }}>秋然长粒香大米5kg/袋</Text>
+                      </View>
+                      <View className="right-content-text-tag">返 ¥9.10</View>
+                      <View className="right-content-text-footer">
+                        <Price price={11} size="normal" needSymbol thousands />
+                        <Icon name="cart" size={15}></Icon>
+                      </View>
                     </View>
                   </View>
                 </View>
-              </View>
+                <View className="right-content-box">
+                  <View className="right-content-item">
+                    <Image
+                      mode="widthFix"
+                      // eslint-disable-next-line global-require
+                      src={require('@/assets/images/homebg.png')}
+                      className="rightImage"
+                    ></Image>
+                    <View className="right-content-text-box">
+                      <View className="right-content-text-header">
+                        <Tag color="rgb(170, 170, 170)">自营</Tag>
+                        <Text style={{ marginLeft: 10 }}>秋然长粒香大米5kg/袋</Text>
+                      </View>
+                      <View className="right-content-text-tag">返 ¥9.10</View>
+                      <View className="right-content-text-footer">
+                        <Price price={11} size="normal" needSymbol thousands />
+                        <Icon name="cart" size={15}></Icon>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </scroll-view>
             </View>
           );
         })}
