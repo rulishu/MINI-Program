6d732f56d1fd42a1b9ca36f2e88c300a380be384
panbibi <1265629139@qq.com>
2023-05-30 09:47:32 +0800

    fix: 【分享弹窗】原型图更新 #9

--- src/pages/goodInfo/share.jsx
+++ src/pages/goodInfo/share.jsx
@@ -1,140 +1,78 @@
 import React from 'react';
-import { View, Text, Image } from '@tarojs/components';
+import { View, Text } from '@tarojs/components';
 import { Popup, Button } from '@nutui/nutui-react-taro';
 import { useDispatch, useSelector } from 'react-redux';
-// import Taro from '@tarojs/taro';
-import goodInfoShare from '@/assets/images/goodInfoShare.svg';
-import goodInfoCollect from '@/assets/images/goodInfoCollect.svg';
-import goodInfoDownload from '@/assets/images/goodInfoDownload.svg';
+import Taro from '@tarojs/taro';
 import './index.scss';
 
 const Index = () => {
   const { shareVisible } = useSelector((state) => state.goodInfo);
   const dispatch = useDispatch();
 
-  const shareList = [
-    {
-      id: 1,
-      img: goodInfoShare,
-      title: '分享给朋友',
-      onClick: () => onShare(),
-    },
-    {
-      id: 2,
-      img: goodInfoCollect,
-      title: '收藏',
-      onClick: () => onCollect(),
-    },
-    {
-      id: 3,
-      img: goodInfoDownload,
-      title: '保存到相册',
-      onClick: () => onDownload(),
-    },
-  ];
-
-  // const onClickChat = () => {
-  //   // Taro.showModal({
-  //   //   title: '提示',
-  //   //   content: '请手动打开微信消息列表',
-  //   //   confirmText: '好的',
-  //   //   showCancel: false
-  //   // })
-  //   Taro.navigateToMiniProgram({
-  //     appId: 'wxeb490c6f9b154ef9', // 要跳转的公众号appid
-  //     path: 'pages/chat/chat?toUserName=xxxxx', // 要打开的聊天页面路径，toUserName为对方用户名或公众号原始id
-  //     extraData: {
-  //       foo: 'bar', // 自定义参数，可选
-  //     },
-  //     // success(res) {
-  //     // }
-  //   });
-  // };
-
-  // 分享给朋友
-  const onShare = () => {
+  // 分享
+  const onClickChat = () => {
     wx.updateShareMenu({
       withShareTicket: true,
-      success: function (res) {
-        window.console.log('分享成功', res);
+      success: function () {
+        Taro.showToast({
+          title: '分享成功',
+          icon: 'success',
+          duration: 2000,
+        });
+        dispatch({
+          type: 'goodInfo/update',
+          payload: { shareVisible: false },
+        });
       },
     });
   };
 
-  // 收藏
-  const onCollect = () => {};
+  // 保存海报
+  const onPoster = () => {
+    dispatch({
+      type: 'goodInfo/update',
+      payload: {
+        shareVisible: false,
+        posterVisible: true,
+      },
+    });
+    // Taro.previewImage({
+    //   current: '', // 当前显示图片的http链接
+    //   urls: images // 需要预览的图片http链接列表
+    // })
+  };
 
-  // 保存到相册
-  const onDownload = () => {};
   return (
-    <Popup
-      visible={shareVisible}
-      position="bottom"
-      style={{ height: '24%' }}
-      onClose={() => dispatch({ type: 'goodInfo/update', payload: { shareVisible: false } })}
-    >
-      <View className="share-box">
-        <View className="share-box-top">
-          {shareList.map((a) => {
-            if (a.title === '分享给朋友') {
-              return (
-                <View key={a.id} className="share-box-share" onTap={a?.onClick}>
-                  <Button className="share-box-share-img share-box-share-botton" open-type="share">
-                    <Image mode="widthFix" src={a.img} className="share-img" />
-                  </Button>
-                  <View className="share-box-share-text">
-                    <Text>{a.title}</Text>
-                  </View>
-                </View>
-              );
-            } else {
-              return (
-                <View key={a.id} className="share-box-share" onTap={a?.onClick}>
-                  <View className="share-box-share-img">
-                    <Image mode="widthFix" src={a.img} className="share-img" />
-                  </View>
-                  <View className="share-box-share-text">
-                    <Text>{a.title}</Text>
-                  </View>
-                </View>
-              );
-            }
-          })}
-        </View>
-        {/* <View className="share-image">
-          <View
-            className="share-image-item1"
-            onClick={() => {
-              onClickChat();
-            }}
-          >
-            <Text>微信</Text>
+    <>
+      <Popup
+        visible={shareVisible}
+        position="bottom"
+        style={{ height: '20%', display: 'flex', justifyContent: 'center' }}
+        onClose={() => dispatch({ type: 'goodInfo/update', payload: { shareVisible: false } })}
+      >
+        <View className="share-box">
+          <View className="share-image-item1-info" onClick={() => onClickChat()}>
+            <View className="share-image-item1">
+              <Button
+                shape="square"
+                color="#D7D7D7"
+                className="share-image-item1 share-box-share-botton"
+                open-type="share"
+              />
+            </View>
+            <View className="share-image-item1-info text">
+              <Text>微信</Text>
+            </View>
           </View>
-          <View
-            className="share-image-item2"
-            onClick={() => {
-              Taro.showModal({
-                title: '保存图片',
-                // content: '请手动打开微信消息列表',
-                confirmText: '取消',
-                showCancel: false,
-              });
-            }}
-          >
-            <Text>海报</Text>
+          <View className="share-image-item1-info" onClick={() => onPoster()}>
+            <View className="share-image-item1"></View>
+            <View className="share-image-item1-info text">
+              <Text>保存海报</Text>
+            </View>
           </View>
-        </View> */}
-        <View className="share-box-bottom">
-          <Button
-            className="botton"
-            shape="square"
-            onClick={() => dispatch({ type: 'goodInfo/update', payload: { shareVisible: false } })}
-          >
-            取消
-          </Button>
         </View>
-      </View>
-    </Popup>
+      </Popup>
+    </>
   );
 };
 export default Index;
