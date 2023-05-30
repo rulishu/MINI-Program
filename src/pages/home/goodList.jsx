dc7589f0325523dab16f6c25eae4c5647bbc92b9
panbibi <1265629139@qq.com>
2023-05-30 15:10:27 +0800

    fix:【首页】联调banner轮播图、活动列表、一级分类、商品列表接口联调 #6

--- src/pages/home/goodList.jsx
+++ src/pages/home/goodList.jsx
@@ -2,45 +2,66 @@ import React from 'react';
 import { View, Image, Text } from '@tarojs/components';
 import homeAdd from '@/assets/images/homeAdd.svg';
 import homeNoAdd from '@/assets/images/homeNoAdd.svg';
+import { Skeleton } from '@nutui/nutui-react-taro';
+import { useDispatch } from 'react-redux';
+import Taro from '@tarojs/taro';
 import './index.scss';
 
 const GoodList = (props) => {
+  const dispatch = useDispatch();
   const { dataList } = props;
+
+  // 跳转商品详情
+  const goGoodInfo = async (itm) => {
+    await dispatch({
+      type: 'goodInfo/infoDetails',
+      payload: {
+        id: itm?.id,
+      },
+    });
+    Taro.navigateTo({ url: '/pages/goodInfo/index' });
+  };
 
   return (
     <View className="list">
       {dataList.map((item) => {
         return (
-          <View className="middle-search-result-info-item" key={item.id}>
+          <View
+            className="middle-search-result-info-item"
+            key={item.id}
+            onTap={() => goGoodInfo(item)}
+          >
             <View className="search-result-image">
-              <Image mode="widthFix" src={item.mainGraph} className="image"></Image>
-
-              {item.goodType && (
+              <Skeleton
+                loading={item.mainGraph ? true : false}
+                width="auto"
+                height="auto"
+                title
+                animated
+                className="search-result-image"
+              >
+                <Image mode="widthFix" src={item.mainGraph} className="image"></Image>
+              </Skeleton>
+              {item.stock === 0 && (
                 <View className="image-state">
-                  <Text>
-                    {item.goodType === 1 ? '异常状态' : item.goodType === 2 ? '已售空' : ''}
-                  </Text>
+                  <Text>{item.stock === 0 ? '已售空' : ''}</Text>
                 </View>
               )}
             </View>
             <View className="search-result-content">
               <View className="search-result-content-head">
-                <Text className="tag">
-                  {item.itemType === 1 ? '自营' : item.itemType === 2 ? '严选' : ''}
-                </Text>
+                <Text className="tag">{item.suppliersId === 1 ? '自营' : '严选'}</Text>
                 <Text className="title">{item.itemName}</Text>
               </View>
               <View className="search-result-content-middle">
-                {item?.activity === '' ||
-                item?.activity === null ||
-                item?.activity === undefined ? (
-                    <Text></Text>
-                  ) : (
-                    <>
-                      <Text className="activity">{item.activity}</Text>
-                      <Text className="activity-price">{item.activityPrice}</Text>
-                    </>
-                  )}
+                {/* {item?.activity === '' ||
+                  item?.activity === null ||
+                  item?.activity === undefined ? (<Text></Text>) : ( */}
+                <>
+                  <Text className="activity">自营</Text>
+                  <Text className="activity-price">¥ 20.00</Text>
+                </>
+                {/* ) } */}
               </View>
               <View className="search-result-content-bottom">
                 <View>
@@ -56,7 +77,7 @@ const GoodList = (props) => {
                 <View className="searchCart">
                   <Image
                     mode="widthFix"
-                    src={item.goodType === 1 || item.goodType === 2 ? homeNoAdd : homeAdd}
+                    src={item.stock === 0 ? homeNoAdd : homeAdd}
                     className="searchCart"
                   ></Image>
                 </View>
--- src/pages/home/navs.jsx
+++ src/pages/home/navs.jsx
@@ -1,46 +1,117 @@
 import React from 'react';
-import { View, Text } from '@tarojs/components'; //Image
-import { data } from './item';
+import { View, Image } from '@tarojs/components';
+import { useSelector } from 'react-redux';
+import { Skeleton } from '@nutui/nutui-react-taro';
 import './index.scss';
 
 const Navs = () => {
+  const { activityList } = useSelector((state) => state.home);
+  // console.log('activityList', activityList, activityList?.map(a => a.path))
+
+  // 点击活动图跳转
+  const goActivity = (jumpPath) => {
+    window.console.log('点击活动图跳转', jumpPath);
+  };
+
   return (
     <View className="nav">
-      <View className="nav-activity">
-        <Text>{data?.activityList?.at(0)?.title}</Text>
-        {/* <Image src={data?.activityList?.at(0)?.carousel} style={{ width: '100%', height: '100%' }} /> */}
-      </View>
+      <Skeleton
+        loading={activityList?.at(0)?.path !== '' ? true : false}
+        width="auto"
+        height="auto"
+        title
+        animated
+        className="nav-activity"
+      >
+        <View className="nav-activity">
+          <Image
+            src={activityList?.at(0)?.path}
+            style={{ width: '100%', height: '100%' }}
+            onTap={() => goActivity(activityList?.at(0)?.jumpPath)}
+          />
+        </View>
+      </Skeleton>
       <View className="nav-content">
         <View className="nav-escort-clan">
-          <Text>{data?.activityList?.at(1)?.title}</Text>
-          {/* <Image src={data?.activityList?.at(1)?.carousel} style={{ width: '100%', height: '100%' }} /> */}
+          {/* <Skeleton loading={activityList?.at(1)?.path ? true : false} width="auto" height="auto" title animated className="nav-escort-clan"> */}
+          <Image
+            src={activityList?.at(1)?.path}
+            style={{ width: '100%', height: '100%' }}
+            onTap={() => goActivity(activityList?.at(1)?.jumpPath)}
+          />
+          {/* </Skeleton> */}
         </View>
         <View className="nav-content-right">
-          <View className="nav-customize-now">
-            <Text>{data?.activityList?.at(2)?.title}</Text>
-            {/* <Image src={data?.activityList?.at(2)?.carousel} style={{ width: '100%', height: '100%' }} /> */}
-          </View>
-          <View className="nav-member-exclusive">
-            <Text>{data?.activityList?.at(3)?.title}</Text>
-            {/* <Image src={data?.activityList?.at(3)?.carousel} style={{ width: '100%', height: '100%' }} /> */}
-          </View>
+          <Skeleton
+            loading={activityList?.at(2)?.path ? true : false}
+            width="auto"
+            height="auto"
+            title
+            animated
+            className="nav-customize-now"
+          >
+            <View className="nav-customize-now">
+              <Image
+                src={activityList?.at(2)?.path}
+                style={{ width: '100%', height: '100%' }}
+                onTap={() => goActivity(activityList?.at(2)?.jumpPath)}
+              />
+            </View>
+          </Skeleton>
+          <Skeleton
+            loading={activityList?.at(3)?.path ? true : false}
+            width="auto"
+            height="auto"
+            title
+            animated
+            className="nav-member-exclusive"
+          >
+            <View className="nav-member-exclusive">
+              <Image
+                src={activityList?.at(3)?.path}
+                style={{ width: '100%', height: '100%' }}
+                onTap={() => goActivity(activityList?.at(3)?.jumpPath)}
+              />
+            </View>
+          </Skeleton>
         </View>
       </View>
       <View className="nav-content-foot1">
         <View className="nav-self-operated-zone">
-          <Text>{data?.activityList?.at(4)?.title}</Text>
-          {/* <Image src={data?.activityList?.at(4)?.carousel} style={{ width: '100%', height: '100%' }} /> */}
+          {/* <Skeleton loading={activityList?.at(4)?.path ? true : false} width="auto" height="auto" title animated className="nav-self-operated-zone"> */}
+          <Image
+            src={activityList?.at(4)?.path}
+            style={{ width: '100%', height: '100%' }}
+            onTap={() => goActivity(activityList?.at(4)?.jumpPath)}
+          />
+          {/* </Skeleton> */}
         </View>
         <View className="nav-self-operated-zone">
-          {/* <Text>镖族严选</Text> */}
-          <Text>{data?.activityList?.at(5)?.title}</Text>
-          {/* <Image src={data?.activityList?.at(5)?.carousel} style={{ width: '100%', height: '100%' }} /> */}
+          {/* <Skeleton loading={activityList?.at(5)?.path ? true : false} width="auto" height="auto" title animated className="nav-self-operated-zone"> */}
+          <Image
+            src={activityList?.at(5)?.path}
+            style={{ width: '100%', height: '100%' }}
+            onTap={() => goActivity(activityList?.at(5)?.jumpPath)}
+          />
+          {/* </Skeleton> */}
         </View>
       </View>
-      <View className="nav-content-foot2">
-        <Text>{data?.activityList?.at(6)?.title}</Text>
-        {/* <Image src={data?.activityList?.at(6)?.carousel} style={{ width: '100%', height: '100%' }} /> */}
-      </View>
+      <Skeleton
+        loading={activityList?.at(6)?.path ? true : false}
+        width="auto"
+        height="auto"
+        title
+        animated
+        className="nav-content-foot2"
+      >
+        <View className="nav-content-foot2">
+          <Image
+            src={activityList?.at(6)?.path}
+            style={{ width: '100%', height: '100%' }}
+            onTap={() => goActivity(activityList?.at(6)?.jumpPath)}
+          />
+        </View>
+      </Skeleton>
     </View>
   );
 };
