27d1dcf921e2bfa0df5d5cdc3c1710dde3278f60
shenjiancheng <1311703897@qq.com>
2023-06-21 16:50:04 +0800

    fix: 优化物流信息界面

--- src/pages/logisticsInfo/index.jsx
+++ src/pages/logisticsInfo/index.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import { View, Image } from '@tarojs/components';
 import Taro from '@tarojs/taro';
-import { Steps, Step } from '@nutui/nutui-react-taro';
+import { Steps, Step, Empty } from '@nutui/nutui-react-taro';
 import Tabs from '@/component/aTabs';
 import { useRequest } from 'ahooks';
 import { useSelector, useDispatch } from 'react-redux';
@@ -44,9 +44,70 @@ const Index = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const LogisticsInfo = (tabItem) => {
+    return (
+      <View className="logisticsInfo">
+        <View className="logisticsInfo-head">
+          {tabItem.items?.map((goodsItem) => {
+            return (
+              <View key={goodsItem} className="logisticsInfo-img">
+                <Image className="logisticsInfo-img" src={goodsItem?.mainGraph}></Image>
+              </View>
+            );
+          })}
+        </View>
+        {tabItem.logisticsCompany ? (
+          <View className="logisticsInfo-cont">
+            <View className="logisticsInfo-content">
+              <View className="logisticsInfo-content-head">
+                <View>
+                  {tabItem.logisticsCompany} {tabItem.trackingNumber}
+                </View>
+                <View
+                  style={{ marginLeft: 10, color: '#965A3C' }}
+                  onClick={() => {
+                    wx.setClipboardData({
+                      data: tabItem.trackingNumber,
+                    });
+                  }}
+                >
+                  复制
+                </View>
+              </View>
+              <View className="logisticsInfo-content-middle">
+                <View className="steps-wrapper">
+                  <Steps direction="vertical" progressDot current={5}>
+                    {logisticsList.map((item) => (
+                      <Step
+                        key={item.id}
+                        icon={item.icon}
+                        iconColor={item.icon === 'check-checked' ? '#222B45' : '#A85230'}
+                        size="14"
+                        activeIndex={item.id}
+                        title={
+                          <View className="title">
+                            <p className="state">{item.state}</p>
+                            <p className="time">{item.time}</p>
+                          </View>
+                        }
+                        content={<View className="content">{item.content}</View>}
+                      ></Step>
+                    ))}
+                  </Steps>
+                </View>
+              </View>
+            </View>
+          </View>
+        ) : (
+          <Empty description="无数据" className="empty" />
+        )}
+      </View>
+    );
+  };
+
   return (
     <View className="index">
-      {packageList?.length !== 0 && (
+      {packageList?.length > 1 ? (
         <Tabs
           value={packageActive}
           onChange={(paneKey) => {
@@ -57,65 +118,14 @@ const Index = () => {
           tabList={packageList?.map((tabItem) => {
             return {
               ...tabItem,
-              children: (
-                <View className="logisticsInfo">
-                  <View className="logisticsInfo-head">
-                    {tabItem.items?.map((goodsItem) => {
-                      return (
-                        <View key={goodsItem} className="logisticsInfo-img">
-                          <Image className="logisticsInfo-img" src={goodsItem?.mainGraph}></Image>
-                        </View>
-                      );
-                    })}
-                  </View>
-                  {tabItem.logisticsCompany && (
-                    <View className="logisticsInfo-cont">
-                      <View className="logisticsInfo-content">
-                        <View className="logisticsInfo-content-head">
-                          <View>
-                            {tabItem.logisticsCompany} {tabItem.trackingNumber}
-                          </View>
-                          <View
-                            style={{ marginLeft: 10, color: '#965A3C' }}
-                            onClick={() => {
-                              wx.setClipboardData({
-                                data: tabItem.trackingNumber,
-                              });
-                            }}
-                          >
-                            复制
-                          </View>
-                        </View>
-                        <View className="logisticsInfo-content-middle">
-                          <View className="steps-wrapper">
-                            <Steps direction="vertical" progressDot current={5}>
-                              {logisticsList.map((item) => (
-                                <Step
-                                  key={item.id}
-                                  icon={item.icon}
-                                  iconColor={item.icon === 'check-checked' ? '#222B45' : '#A85230'}
-                                  size="14"
-                                  activeIndex={item.id}
-                                  title={
-                                    <View className="title">
-                                      <p className="state">{item.state}</p>
-                                      <p className="time">{item.time}</p>
-                                    </View>
-                                  }
-                                  content={<View className="content">{item.content}</View>}
-                                ></Step>
-                              ))}
-                            </Steps>
-                          </View>
-                        </View>
-                      </View>
-                    </View>
-                  )}
-                </View>
-              ),
+              children: LogisticsInfo(tabItem),
             };
           })}
         />
+      ) : packageList?.length !== 0 ? (
+        LogisticsInfo(packageList[0])
+      ) : (
+        <Empty description="无数据" className="empty" />
       )}
     </View>
   );
