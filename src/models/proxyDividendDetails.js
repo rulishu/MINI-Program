4c7dedd7c715d1284b099f8e1ce571c993591ae3
rulishu <1324214367@qq.com>
2023-07-14 13:19:29 +0800

    fix: 调用代理管理分润明细查询接口

--- src/models/proxyDividendDetails.js
+++ src/models/proxyDividendDetails.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+import Taro from '@tarojs/taro';
+import { agentSelectDetail, agentSelectList } from '@/server/proxyDividendDetails';
 
 export default {
   namespace: 'proxyDividendDetails',
@@ -6,9 +8,26 @@ export default {
     // 初始化数据
     popupOpen: false,
     orderActive: 0,
+    detailData: {}, // 分润明细查询数据
   },
 
-  effects: {},
+  effects: {
+    // 代理管理分润明细查询
+    *agentSelectDetail({ payload }, { call, put }) {
+      try {
+        const params = { ...payload };
+        const result = yield call(agentSelectDetail, params);
+        if (result && result.code === 200) {
+          yield put({
+            type: 'update',
+            payload: {
+              detailData: result?.result,
+            },
+          });
+        }
+      } catch (err) {}
+    },
+  },
   reducers: {
     update(state, { payload }) {
       return { ...state, ...payload };
